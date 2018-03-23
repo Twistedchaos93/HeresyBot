6637bc919c5016f064607c3f355e1f0245c35e10
Glitch (heresybot) <none>
2018-03-23 21:10:22 +0000

    🙅🚇 Updated with Glitch

--- ppa.js
+++ ppa.js
@@ -21,28 +21,44 @@ var prefix = "~";
 var msgContents;
 var swears = ["fuck", "shit", "dammit", "damn", "bitch", "ech"];
 var theList = [];
+//game vars
+var playerCount = 0;
+var playerList = [];
+var targetList = [];
+var joinFlag;
+var gameStarted = false;
+
+
 client.on('message', msg => {
+  if(msg.channel.type == "dm"){
+     return;
+  }
   const Heresy = msg.guild.emojis.find("name", "Heresy");
-  const ChristianServer = msg.guild.emojis.find("name", "ChristianServer");
+  const ChristianServer = msg.client.emojis.find("name", "ChristianServer");
   if (msg.author === client.user) return;
   msgContents = msg.content;
 
-
 // Silly stuff without prefix
+  var repliedYet = false;
   if (testList(swears)) {
     msg.channel.send(ChristianServer.toString()+' YOU HECKIN SWORE '+ ChristianServer.toString());
   }
   if(msgContents.toLowerCase().includes("heresy")){
     msg.channel.send('Did somebody say heresy? '+ Heresy.toString());
+    repliedYet = true;
   }
   if(msgContents.toLowerCase() === "arrays start at one" || msgContents.toLowerCase() === "arrays start at 1"){
     msg.channel.send("Of course! Why wouldn't they?");
+    repliedYet = true;
+  }
+  if(repliedYet){
+   return; 
   }
 // Prefix based stuff
   if (msg.content.startsWith(prefix)){
     switch(msgContents){
       case "~test":
-        msg.channel.send("It worked!");
+        msg.author.send("Hello!");
       break;
       //The List
       case "~addmetothelist":
@@ -61,17 +77,138 @@ client.on('message', msg => {
         msg.channel.send("Confirmed");
       break;
       //End The List
+      case "~playercount":
+        msg.channel.send(playerList.length+" players have joined");
+      break;
+      case "~joingame":
+        if(!gameStarted){
+        joinFlag = false;
+          if(playerList.length !== 0){
+            for(var i = 0; i<playerList.length; i++){
+              if(msg.author === playerList[i]){
+                joinFlag = true;
+              }
+            }
+          }
+        if(joinFlag){
+            msg.channel.send("You've already joined");
+        }else{
+          playerList[playerCount] = msg.author;
+          playerCount++;
+          msg.channel.send("Confirmed");  
+        }  
+        }else{
+          msg.channel.send("Game is in progress"); 
+        }
+      break;
+      case "~leavegame":
+        if (!gameStarted){
+          if (playerList.length === 0){
+            return msg.channel.send("You aren't in the game");
+          }
+          joinFlag = false;
+        var i = -1;
+        while(joinFlag == false && i < playerList.length){
+          i++;
+          if(msg.author === playerList[i]){
+            joinFlag = true;
+          }
+        }
+        if(joinFlag){
+          //remove from array
+          playerList.splice(i,1);
+          msg.channel.send("You are no longer in the game");
+        }else{
+          msg.channel.send("You aren't in the game");
+        }
+        }else{
+         msg.channel.send("Game is in progress"); 
+        }
+      break;
+      case "~gamestart":
+        if(inGame(msg.author)){
+           startGame();
+           msg.channel.send("Game started");
+        }else{
+          msg.channel.send("Only player can start the game");
+        }
+      break;
+      case "~gameend":
+        if (gameStarted){
+          if(inGame(msg.author)){
+
+            while (playerList.length !== 0){
+            playerList.pop(); 
+            }
+            gameStarted = false;
+            msg.channel.send("Game ended");
+          }else{
+            msg.channel.send("Only player can start the game");
+          }
+        }else{
+          msg.channel.send("Game is not in progress");
+        }
+      break;
+      case "~imdead":
+        if(inGame(msg.author)){
+          var newTarget;
+          var j;
+          var hunter;
+          for(j = 0; j<playerList.length; j++){
+            if(msg.author == playerList[i]){
+              if(j+1 == playerList.length){
+                newTarget = 0;
+              }else{
+                newTarget = j+1;
+              }
+              if(j === 0){
+                hunter = playerList.length-1;
+              }else{
+                hunter = j-1;
+              }
+              playerList[hunter].send("Your new target is " + playerList[newTarget]);
+              playerList.splice(j, 1);
+            }
+          }
+          msg.channel.send("RIP");
+        }else{
+          msg.channel.send("You aren't playing");
+        }
+      break;
       default:
         msg.channel.send("I don't understand");
     }
   }
 
 });
+
 client.login(process.env.TOKEN);
 
 function testList(array){
   for(var i = 0; i<array.length; i++){
     if (msgContents.toLowerCase().includes(array[i])){
+      return(true);
+    }
+  }
+  return(false);
+}
+function startGame(){
+  var targetPlaceholder;
+    // sorts the playerlist array randomly
+    playerList.sort(function(a, b){return 0.5 - Math.random()});
+    for(var i = 0; i<playerList.length; i++){
+      if(i+1 == playerList.length){
+         targetPlaceholder = 0;
+      }else{
+         targetPlaceholder = i+1;
+      }
+        playerList[i].send("Your target is " + playerList[targetPlaceholder].username);
+      }
+      gameStarted = true;  
+}
+function inGame(player){
+  for(var i = 0; i<playerList.length; i++){
+    if (player == playerList[i]){
       return(true);
     }
   }
